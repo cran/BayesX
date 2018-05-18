b31363ce2bc6a7d7d0c9c77b7243c131e68f8dee
Nikolaus Umlauf <Nikolaus.Umlauf@uibk.ac.at>
2018-05-18 04:07:54 +0000

    version 0.3-0

--- R/fuse.R
+++ R/fuse.R
@@ -2,63 +2,62 @@ fuse <- function(map, regions, name) {
   
   regions <- regions
   testmap <- map
-  surroundingNames <-(attributes(map)$surrounding)
-  surroundingNames2 <-unlist(attributes(map)$surrounding)
+  surroundingNames <- (attributes(map)$surrounding)
+  surroundingNames2 <- unlist(attributes(map)$surrounding)
   ind <- c()
   indexrem <- NULL
   j <- 0
   regisurrounding <- c()
-  for(i in 1:length(surroundingNames)) {
+  for (i in 1:length(surroundingNames)) {
     
-    if(length(surroundingNames[[i]])>0) {
-      j <- j+1
-      if(any(regions==surroundingNames2[j])) {
-        if(any(regions==names(map)[i])) {
+    if (length(surroundingNames[[i]]) > 0) {
+      j <- j + 1
+      if (any(regions == surroundingNames2[j])) {
+        if (any(regions == names(map)[i])) {
           
         } else {
-          ind <- c(ind,j)
-          regisurrounding <- c(regisurrounding,names(map)[i])
+          ind <- c(ind, j)
+          regisurrounding <- c(regisurrounding, names(map)[i])
         }
       }
     }
   }
-
+  
   polys <- list()
   id <- c()
-  for(i in 1:length(regions)) {
-    check <- which(names(map)==regions[i])
-    for(l in 1:length(check)) {
-      id <- c(id,paste("p",1+length(id),sep=""))
-      if(any(indexrem==check[l])) {
+  for (i in 1:length(regions)) {
+    check <- which(names(map) == regions[i])
+    for (l in 1:length(check)) {
+      id <- c(id, paste("p", 1 + length(id), sep = ""))
+      if (any(indexrem == check[l])) {
         
-      }else{
-        indexrem <- c(indexrem,check[l])
+      } else {
+        indexrem <- c(indexrem, check[l])
       }
     }
-    if(length(which(names(map)==regions[i]))==0) {
-      warning(paste(regions[i]," not contained in map"),sep="")
+    if (length(which(names(map) == regions[i])) == 0) {
+      warning(paste(regions[i], " not contained in map"), sep = "")
     }
     
   }
   
-  for(i in 1:length(indexrem)) {
+  for (i in 1:length(indexrem)) {
     
-    polys <- c(polys,sp::Polygons(list(sp::Polygon(rbind(map[[indexrem[i]]][dim(map[[indexrem[i]]])[1],],map[[indexrem[i]]]))),id[i]))
+    polys <- c(polys, sp::Polygons(list(sp::Polygon(rbind(map[[indexrem[i]]][dim(map[[indexrem[i]]])[1], 
+      ], map[[indexrem[i]]]))), id[i]))
   }
   
   testmap <- map[-indexrem]
   ntemp <- length(testmap)
-  #combine seleted regions to one single polygon, add this region to map again
+  # combine seleted regions to one single polygon, add this region to map again
   spatpol <- sp::SpatialPolygons(polys)
-  combine <- (maptools::unionSpatialPolygons(spatpol,rep(1,length(slot(spatpol, "polygons")))))
-  regionadd <- slot(combine,"polygons")
+  combine <- (maptools::unionSpatialPolygons(spatpol, rep(1, length(spatpol@polygons))))
+  regionadd <- combine@polygons
   regionadd <- (regionadd[[1]]@Polygons)
-  for(i in 1:length(regionadd)) {
-    testmap[[ntemp+i]] <- regionadd[[i]]@coords
-    names(testmap)[ntemp+i] <- name
+  for (i in 1:length(regionadd)) {
+    testmap[[ntemp + i]] <- regionadd[[i]]@coords
+    names(testmap)[ntemp + i] <- name
   }
-  
-  
   
   minima <- sapply(map, function(x) {
     apply(x, 2, min)
@@ -72,16 +71,16 @@ fuse <- function(map, regions, name) {
   y.range <- maximum[2] - minimum[2]
   height2width <- round(y.range/x.range, digits = 2)
   surrounding <- replicate(n = length(testmap), expr = character())
-  if(length(regisurrounding)>0) {
-    for(i in 1:length(regisurrounding)) {
-      ind2 <- which(names(testmap)==regisurrounding[i])
+  if (length(regisurrounding) > 0) {
+    for (i in 1:length(regisurrounding)) {
+      ind2 <- which(names(testmap) == regisurrounding[i])
       surrounding[[ind2]] <- surroundingNames2[ind[i]]
     }
   }
   regi <- unique(names(testmap))
   
-  attributes(testmap) <- list(names=names(testmap),height2width = height2width, class="bnd",
-                          surrounding = surrounding, regions = regi)
+  attributes(testmap) <- list(names = names(testmap), height2width = height2width, 
+    class = "bnd", surrounding = surrounding, regions = regi)
   
   return(testmap)
   
